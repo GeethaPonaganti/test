c3c9b38be409c9caf460e899fa166e9f656e4098
Veerendra <8393701+veerendra2@users.noreply.github.com>
2017-11-20 14:16:15 +0530

    Update prometheus-pods.py

--- kubernetes/prometheus-pods.py
+++ kubernetes/prometheus-pods.py
@@ -1,23 +1,25 @@
 #!/usr//bin/env python2
 '''
 Author : OpsMx
-Description :  Retrives pods in cluster/Replicasets
+Description :  Retrives pods in cluster/Replicasets from Prometheus
 '''
 
 import json
 import argparse
 import warnings
+import re
 warnings.filterwarnings("ignore")
 
 try:
     import requests
 except ImportError:
-    print "[!] 'requests' module is not found. please install-> 'sudo pip2 install reuests'"
+    print "[!] 'requests' module is not found. please install-> 'sudo pip2 install requests'"
+    exit(1)
 
 
 METRIC_NAME = "container_cpu_user_seconds_total"
-CLUSTER_URL = "http://{}:9090/api/v1/series?match[]=up&match[]={}{{container_label_cluster='{}'}}"
-REPLICASET_URL = "http://{}:9090/api/v1/series?match[]=up&match[]={}{{container_label_replication_controller='{}'}}"
+CLUSTER_URL = "http://{}/api/v1/series?match[]=up&match[]={}{{container_label_cluster='{}'}}"
+REPLICASET_URL = "http://{}/api/v1/series?match[]=up&match[]={}{{container_label_replication_controller='{}'}}"
 
 
 class PrometheusAPI:
@@ -28,7 +30,10 @@ class PrometheusAPI:
     def get_latest_cluster(self, cluster):
         version = dict()
         try:
-            json_data = requests.get(CLUSTER_URL.format(self.serverip, METRIC_NAME, cluster)).json()
+            json_data = requests.get(CLUSTER_URL.format(self.serverip, METRIC_NAME, cluster),  timeout=5).json()
+        except requests.exceptions.ConnectTimeout:
+            print "[!] Connection timeout for {}".format(self.serverip)
+            exit(1)
         except:
             print "[!] Not able to fetch cluster info"
             exit(1)
@@ -41,13 +46,15 @@ class PrometheusAPI:
         if version:
             return version[max(version)]
         else:
-            print "[!] Specified cluster not found"
+            print "[!] Specified cluster is not found"
             exit(1)
 
     def get_pods(self, replicaset_name):
         data =list()
         try:
-            json_data = requests.get(REPLICASET_URL.format(self.serverip, METRIC_NAME, replicaset_name)).json()
+            json_data = requests.get(REPLICASET_URL.format(self.serverip, METRIC_NAME, replicaset_name), timeout=5).json()
+        except requests.exceptions.ConnectTimeout:
+            print "[!] Connection timeout for {}".format(self.serverip)
         except:
             print "[!] Not able to fetch pods info"
             exit(1)
@@ -69,12 +76,21 @@ class PrometheusAPI:
 if __name__ == '__main__':
     NAMESPACE = "default"
     parser = argparse.ArgumentParser(description="Retrives pods in cluster/Replicasets from Prometheus")
-    parser.add_argument("-c", action="store", dest="cls_name", help="Gets the pods in this cluster")
-    parser.add_argument("-C", action="store", dest="current_cls_name", help="Gets the pods in 'current' cluster")
-    parser.add_argument("-s", action="store", dest="serverip", help="Prometheus server IP")
+    parser.add_argument("-c", action="store", dest="cls_name", help="Gets the pods in this cluster(Baseline)")
+    parser.add_argument("-C", action="store", dest="current_cls_name", help="Gets the pods in 'current' cluster(Curent)")
+    parser.add_argument("-s", action="store", dest="serverip", help="Prometheus enpoint. Format SERVERIP:PORT")
     parser.add_argument("-n", action="store", dest="namespace", help="Namespace, Default:'default'")
-    parser.add_argument("-v", action="store_true", dest="validate", default=False, help="Validates the config file")
     options = parser.parse_args()
+    if options.serverip:
+        match = re.findall(r'[0-9]+(?:\.[0-9]+){3}:[0-9]+', options.serverip)
+        if match:
+            SERVERIP = match[0]
+        else:
+            print "[!] Invalid enpoint. The endpoint should be like http:127.0.0.1:9090"
+            exit(1)
+    else:
+        print "[!] Please specify prometheus endpoint. For help-> python pods-prometheus.py -h"
+        exit(1)
     if options.namespace:
         NAMESPACE = options.namespace
     if options.cls_name and options.serverip:
