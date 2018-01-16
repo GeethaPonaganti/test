40df5b2403d1bf3a009b5edddd3b37ff367c3766
Veerendra <veerendrav2@gmail.com>
2018-01-17 00:30:55 +0530

    Update hal-installer-kube.sh

--- spinnaker/hal-installer-kube.sh
+++ spinnaker/hal-installer-kube.sh
@@ -10,12 +10,18 @@ else
 	echo "$file not found. Place the kubernetes authentication file in ~/.kube"
     exit 1
 fi
+echo -n "Enter accessKeyId> "
+read accessKeyId
+echo -n "Enter region> "
+read region
 sudo apt-get update
 sudo apt-get upgrade -y
 curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/stable/InstallHalyard.sh
 sudo bash InstallHalyard.sh
 hal config deploy edit --type localdebian
-hal config storage edit --type redis
+#hal config storage edit --type redis
+hal config storage s3 edit --access-key-id $accessKeyId --secret-access-key --region $region
+hal config storage edit --type s3
 hal config provider docker-registry enable
 hal config provider docker-registry account add my-docker-registry --address $ADDRESS --repositories $REPOSITORIES --username $USERNAME --password
 hal config provider kubernetes enable
